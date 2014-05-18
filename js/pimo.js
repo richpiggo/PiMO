8a8e88ed9896f951f31fa78b5d77eb6344d88c44
Richard Pigott <rich@sylva.org.uk>
2014-05-18 19:02:17 +0100

    fiddled with some libs, nothing much yet

---
+++ js/pimo.js
@@ -0,0 +1,12 @@
+/**
+* PiMO client
+*/
+
+// open soket
+var socket = io.connect("http://localhost");
+
+// news event
+socket.on( "news", function (data) {
+	console.log( data );
+	socket.emit("my other event", { my:"data" });
+});

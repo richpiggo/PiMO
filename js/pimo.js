f0a8289ae1859f32dd9c925c808eb51f89d9ae56
Richard Pigott <rich@sylva.org.uk>
2014-12-19 18:09:20 +0000

    Who wants to play video games?q

--- js/pimo.js
+++ js/pimo.js
@@ -3,10 +3,10 @@
 */
 
 // open soket
-var socket = io.connect("http://localhost");
+// var socket = io.connect("http://localhost");
 
-// news event
-socket.on( "news", function (data) {
-	console.log( data );
-	socket.emit("my other event", { my:"data" });
-});
+// // news event
+// socket.on( "news", function (data) {
+// 	console.log( data );
+// 	socket.emit( "my other event", { my:"data" } );
+// });
